95f8769d081524eaf6adc1fd45d86d24bf518cbe
AmaruB76 <145008356+AmaruB76@users.noreply.github.com>
2023-09-14 05:00:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_00wjrq2", "template_cmhc1qu", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
